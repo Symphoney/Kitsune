654588c7ad59ba6a37f4b7d48332939e84b5b377
Symphoney <rijarahmed1@gmail.com>
2026-04-14 18:21:15 -0400

    Setup

---
+++ include/linked_list.h
@@ -0,0 +1,8 @@
+//
+// Created by Rija on 4/14/2026.
+//
+
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+#endif //LINKEDLIST_H
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Rija on 4/14/2026.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "Test" << '\n';
+    return 0;
+}
