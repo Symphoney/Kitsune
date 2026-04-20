f22350283882ccf1f802d9bc19fe56382bf8b2ef
Symphoney <rijarahmed1@gmail.com>
2026-04-20 01:59:17 -0400

    Stack implementation includes comments to show understanding

--- include/Stack.h
+++ include/Stack.h
@@ -1,6 +1,13 @@
 //
 // Created by Rija on 4/20/2026.
 //
+/*
+ * Stack - LIFO order of insertion and deletion
+ * Most recently inserted element is removed first
+ * The first inserted element will be removed last
+ * Insert and delete elements at only one end of the stack - top
+ *
+ */
 
 #pragma once
 #include <iostream>
@@ -12,10 +19,13 @@ private:
     size_t next_idx_;
 
 public:
+    // Default constructor with initial capacity of 4
     Stack() {
         arr_ = new T[4];
         next_idx_ = 0;
     }
+
+    // Constructor with user-defined initial capacity
     Stack (const size_t sz) {
         arr_ = new T[sz];
         next_idx_ = 0;
@@ -29,6 +39,7 @@ public:
         return next_idx_ == 0;
     }
 
+    // Push - new elements can only be inserted at the top of the stack
     void push(const T data) {
         if (next_idx_ == 4) {
             T* temp = new T[2 * 4];
@@ -45,6 +56,7 @@ public:
         next_idx_++;
     }
 
+    // Pop - only the top element of the stack can be deleted
     void pop() {
         if (next_idx_ == 0) {
             return;
@@ -53,6 +65,7 @@ public:
         next_idx_--;
     }
 
+    // Only the top element can be accessed in a stack
     T top() const {
         return arr_[next_idx_ - 1];
     }
