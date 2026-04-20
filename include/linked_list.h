bdd15a11261ae1d21faec313002c73291fadb879
Symphoney <rijarahmed1@gmail.com>
2026-04-20 01:37:40 -0400

    Added in queue

--- include/linked_list.h
+++ include/linked_list.h
@@ -2,7 +2,146 @@
 // Created by Rija on 4/14/2026.
 //
 
-#ifndef LINKEDLIST_H
-#define LINKEDLIST_H
+#pragma once
+#include <iostream>
+#include <type_traits>
 
-#endif //LINKEDLIST_H
+template <typename T, typename = std::enable_if_t<std::is_integral_v<T>>>
+
+struct ListNode {
+    T data;
+    ListNode* next;
+
+    ListNode(const T n) {
+        data = n;
+        next = nullptr;
+    }
+
+    void insert(const T n) {
+        ListNode* node = new ListNode(n);
+        ListNode* curr = this;
+
+        while (curr->next != nullptr) {
+            curr = curr->next;
+        }
+
+        curr->next = node;
+    }
+
+    void print() const {
+        ListNode* curr = this;
+
+        while (curr != nullptr) {
+            std::cout << curr->data << ' ';
+            curr = curr->next;
+        }
+
+        std::cout << '\n';
+    }
+
+    void remove(const T n) {
+        ListNode* curr = this;
+        ListNode* prev = nullptr;
+
+        while (curr != nullptr) {
+            if (curr->data == n) {
+                if (prev == nullptr) {
+                    data = curr->next->data;
+                    next = curr->next->next;
+                }
+                else {
+                    prev->next = curr->next;
+                }
+
+                delete curr;
+                return;
+            }
+
+            prev = curr;
+            curr = curr->next;
+        }
+    }
+
+    void reverse() {
+        ListNode* curr = this;
+        ListNode* prev = nullptr;
+        ListNode* next_node  = nullptr;
+
+        while (curr != nullptr) {
+            next_node = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next_node;
+        }
+
+        data = prev->data;
+        next = prev->next;
+    }
+
+    int length() const {
+        ListNode* curr = this;
+        int count = 0;
+
+        while (curr != nullptr) {
+            count++;
+            curr = curr->next;
+        }
+
+        return count;
+    }
+
+    ListNode* middle() {
+        ListNode* slow = this;
+        ListNode* fast = this;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return slow;
+    }
+
+    ListNode* nth(const size_t n) {
+        ListNode* curr = this;
+        size_t count = 0;
+
+        while (curr != nullptr) {
+            if (count == n) {
+                return curr;
+            }
+
+            count++;
+            curr = curr->next;
+        }
+
+        return nullptr;
+    }
+
+    ListNode* merge(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode(0);
+        ListNode* curr = dummy;
+
+        while (l1 != nullptr && l2 != nullptr) {
+            if (l1->data < l2->data) {
+                curr->next = l1;
+                l1 = l1->next;
+            }
+            else {
+                curr->next = l2;
+                l2 = l2->next;
+            }
+
+            curr = curr->next;
+        }
+
+        if (l1 != nullptr) {
+            curr->next = l1;
+        }
+        else {
+            curr->next = l2;
+        }
+
+        return dummy->next;
+    }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -4,92 +4,22 @@
 
 #include <iostream>
 #include <array>
-
-namespace dsa {
-    // Node class
-    template <typename T>
-    class Node {
-    public:
-        // Properties
-        T data;
-        Node<T> *next;
-
-        // Constructor
-        Node() : data(NULL), next(NULL){};
-        Node(T x) : data(x), next(NULL){};
-
-        void setData(T data) {
-            this->data = data;
-        }
-
-        T getData() {
-            return this->data;
-        }
-
-        void setNext(Node<T> next) {
-            this->next = next;
-        }
-
-        T getNext() {
-            return this->next;
-        }
-    };
-    // Linkedlist class
-    template <typename T>
-    class LinkedList {
-    public:
-        // Properties
-        Node<T> *head;
-        Node<T> *tail;
-        int size;
-
-        // Constructor
-        LinkedList() : head(NULL), tail(NULL), size(0){};
-
-        // Functions
-        void addToList(T data) {
-            Node<T> *newNode = new Node<T>(data);
-            // List is empty
-            if (size == 0) {
-                this->head = newNode;
-                this->tail = newNode;
-                this->head->next = tail;
-                this->size++;
-            }
-            else {
-                this->tail->next = newNode;
-                this->tail = newNode;
-                this->size++;
-            }
-
-
-            // List is not empty
-        }
-
-        void printList() {
-            Node<T> *tempNode = this->head;
-
-            while(tempNode->next != NULL) {
-                std::cout << tempNode->data << "\n";
-                tempNode = tempNode->next;
-            }
-            std::cout << tempNode->data << "\n";
-        }
-    };
-
-};
+#include "Queue.h"
 
 int main() {
-    std::cout << "Test" << '\n';
-    std::array<std::string, 5> mytest = {"I", "AM", "TESTING", "MY", "CODE"};;
-    std::array<int, 5> mytest2 = {1, 2, 3, 4, 5};
+        Queue<int> q;
 
-    dsa::LinkedList<std::string> myList;
+        q.enqueue(1);
+        q.enqueue(2);
+        q.enqueue(3);
+        q.enqueue(4);
 
-    for (std::string i : mytest) {
-        myList.addToList(i);
-    }
+        std::cout << "Size: " << q.size() << '\n';
 
-    myList.printList();
+        q.dequeue();
+        q.dequeue();
+
+        std::cout << "Size: " << q.size() << '\n';
+
     return 0;
 }
